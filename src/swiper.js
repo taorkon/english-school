64b293d2ffead56a83caa007e230b0e4d14e8593
LoveBosikova <117667170+LoveBosikova@users.noreply.github.com>
2023-04-17 17:54:00 +0300

    Dev liuba - courses: html and primary styles done, new frame done (#4)
    * new block feedback
    
    * some stiles added, swiper tried  hard but wiyhout success
    
    * feedback styles in common styles
    
    * image style fixed
    
    * little BEM restyle
    
    * removed unused classes, added formulas for width
    
    * slider fixed, adaptive, setted. animation header + animation photo developed
    
    * courses: html and primary styles done
    
    * fixes after unfortunate merged conflict, swiper broken
    
    * swiper fixed
    
    * html coutses totally done for every onecourse
    
    * puctures added - courses
    
    ---------
    
    Co-authored-by: taorkon <taorkon.tootta@gmail.com>

--- src/swiper.js
+++ src/swiper.js
@@ -1,5 +1,43 @@
-new Swiper(".feedback", {
+new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
+  slidesPerView: 1,
+  loop: true,
+  speed: 500,
+  spaceBetween: 10,
+
+  autoplay: {
+    delay: 20000,
+    stopOnLastSlide: true,
+    disableOnInteraction: true,
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+    eventsTarget: ".guest_reviews",
+  },
+
+  simulateTouch: true,
+  grabCursor: true,
+
+  watchOverflow: true,
+
+  breakpoints: {
+    420: {
+      slidesPerView: 1.5,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1124: {
+      slidesPerView: 2.5,
+    },
+  },
 });
