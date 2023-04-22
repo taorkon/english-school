b359ac23974808ad72d390e99bf7946bab3d300e
MariIvanova <irairina411@gmail.com>
2023-04-22 18:58:49 +0300

    modified block

---
+++ diploma.js
@@ -0,0 +1,83 @@
+
+function ShowModal(elId) {
+    var modalAll = document.getElementById(elId);
+    modalAll.style.display = "flex";
+  }
+  
+  function HideModal() {
+    if (event.target.classList.contains('modal')) {
+      event.currentTarget.style.display = "none";
+    }
+  }
+
+
+  function ShowModal1(elId) {
+    var modalAll = document.getElementById(elId);
+    modalAll.style.display = "flex";
+  }
+  
+  function HideModal1() {
+    if (event.target.classList.contains('modal1')) {
+      event.currentTarget.style.display = "none";
+    }
+  }
+
+
+  function ShowModal2(elId) {
+    var modalAll = document.getElementById(elId);
+    modalAll.style.display = "flex";
+  }
+  
+  function HideModal2() {
+    if (event.target.classList.contains('modal2')) {
+      event.currentTarget.style.display = "none";
+    }
+  }
+
+
+  function ShowModal3(elId) {
+    var modalAll = document.getElementById(elId);
+    modalAll.style.display = "flex";
+  }
+  
+  function HideModal3() {
+    if (event.target.classList.contains('modal3')) {
+      event.currentTarget.style.display = "none";
+    }
+  }
+
+
+  function ShowModal4(elId) {
+    var modalAll = document.getElementById(elId);
+    modalAll.style.display = "flex";
+  }
+  
+  function HideModal4() {
+    if (event.target.classList.contains('modal4')) {
+      event.currentTarget.style.display = "none";
+    }
+  }
+
+
+  function ShowModal5(elId) {
+    var modalAll = document.getElementById(elId);
+    modalAll.style.display = "flex";
+  }
+  
+  function HideModal5() {
+    if (event.target.classList.contains('modal5')) {
+      event.currentTarget.style.display = "none";
+    }
+  }
+
+
+  function ShowModal6(elId) {
+    var modalAll = document.getElementById(elId);
+    modalAll.style.display = "flex";
+  }
+  
+  function HideModal6() {
+    if (event.target.classList.contains('modal6')) {
+      event.currentTarget.style.display = "none";
+    }
+  }
